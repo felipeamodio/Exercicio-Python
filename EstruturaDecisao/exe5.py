c3340a857d081310fdf5cece0475ca94b7521035
Felipe <fealves.amodio@gmail.com>
2021-05-27 19:34:45 -0300

    update exercicio 5

--- EstruturaDecisao/exe5.py
+++ EstruturaDecisao/exe5.py
@@ -2,3 +2,15 @@
 # A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
 # A mensagem "Reprovado", se a média for menor do que sete;
 # A mensagem "Aprovado com Distinção", se a média for igual a dez.
+
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+
+media = (nota1 + nota2) / 2
+
+if media >= 7 and media < 10:
+    print("Parabéns, você foi aprovado com uma media de {}!".format(media))
+elif media < 7:
+    print("Que pena, você foi reprovado! Sua média foi de {}".format(media))
+elif media == 10:
+    print("Wow, que incrível!! Você passou com um 10, parabéns!!!")
